d37503a64a7d0f568330f6dce35fa24d913780d2
AStwo <adrianstworzyjanek@gmail.com>
2020-04-29 21:23:48 +0200

    Add missing mutation methods

--- src/space/SearchSpace.py
+++ src/space/SearchSpace.py
@@ -10,6 +10,9 @@ class Uniform:
     def get_value(self):
         return np.random.uniform(self.min, self.max)
 
+    def mutate_param(self, value):
+        return max(min(value * (1 + np.random.uniform(-0.2, 0.2)), self.max), self.min)
+
 
 class UniformInt:
     def __init__(self, name, min_value, max_value):
@@ -19,5 +22,9 @@ class UniformInt:
 
     def get_value(self):
         return np.random.randint(self.min, self.max)
+
+    def mutate_param(self, value):
+        mutated_value = round(value * np.random.uniform(0, 0.2) * np.random.choice([-1, 1]))
+        return max(min(mutated_value, self.max), self.min)
 
 
